5afdf002c74ec93e2813249848b0f633c43e2d92
panzerstadt <tliqun@gmail.com>
2018-09-05 18:12:56 +0900

    use Radium (library) to include fancy css into inline styles css edits

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Radium from "radium";
 
 import en_lorem_ipsum from "../assets/lorem_ipsum/lorem-ipsum-en.txt";
 import ja_lorem_ipsum from "../assets/lorem_ipsum/lorem-ipsum-ja.txt";
+
+import dude_1 from "../assets/avatar/2Asset 3.svg";
+import dude_2 from "../assets/avatar/2Asset 4.svg";
+import dude_3 from "../assets/avatar/2Asset 10.svg";
 
 function importAll(r) {
   let images = {};
@@ -40,10 +45,10 @@ const p1Style = {
     alignItems: "center",
     flexWrap: "wrap",
     height: "100vh",
-    color: "white",
     paddingTop: 30,
     paddingBottom: 30,
-    backgroundColor: "red"
+    boxShadow: "0 0 50px #EAEFF2",
+    zIndex: 10
   },
   title: {
     fontSize: 25
@@ -75,10 +80,9 @@ const p2Style = {
     alignItems: "center",
     flexWrap: "wrap",
     height: "100vh",
-    color: "white",
     paddingTop: 30,
     paddingBottom: 30,
-    backgroundColor: "grey"
+    backgroundColor: "#FCFEFF"
   },
   title: {
     fontSize: 25
@@ -110,11 +114,23 @@ const p3Style = {
     justifyContent: "center",
     alignItems: "center",
     flexWrap: "wrap",
-    backgroundColor: "white"
+    backgroundColor: "white",
+    boxShadow: "0 0 50px #EAEFF2",
+    zIndex: 10
   },
   text: {
-    fontSize: 20,
+    fontSize: 12,
+    fontWeight: 100,
     width: 300
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 100
+  },
+  textBox: {
+    "@media (min-width: 768px)": {
+      marginLeft: 200
+    }
   },
   image: {
     height: "100%",
@@ -126,22 +142,101 @@ const page_3 = (
   <div style={p3Style.root}>
     <img style={p3Style.image} src={images["bg/path.jpeg"]} />
 
-    <div>
-      <p style={p3Style.text}>test page 3</p>
+    <div style={p3Style.textBox}>
+      <p style={p3Style.title}>How Does it Work?</p>
       <p style={p3Style.text}>{readTextFile(ja_lorem_ipsum)}</p>
     </div>
   </div>
 );
 
-const all_pages = [page_1, page_2, page_3, page_2];
+const p4Style = {
+  root: {
+    // height: 500,
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    flexWrap: "wrap",
+    height: "100vh",
+    backgroundColor: "#FDFDFD",
+    color: "#1E1B0E"
+  },
+  text: {
+    fontSize: 15,
+    width: 300,
+    fontWeight: 100,
+    textShadow: "0 2px 3px #EDEEEF"
+  },
+  title: {
+    fontWeight: 100,
+    textShadow: "0 2px 3px #EDEEEF"
+  },
+  image: {
+    height: 200
+  },
+  avatar_box: {
+    padding: 15
+  },
+  footer: {
+    paddingTop: 30,
+    color: "grey",
+    width: "100%",
+    fontSize: 12,
+    fontWeight: 100
+  }
+};
+
+const avatar_data = [
+  {
+    name: "Rahmat Hidayat",
+    avatar: dude_3,
+    text: readTextFile(en_lorem_ipsum)
+  },
+  {
+    name: "Piotr Kroujkov",
+    avatar: dude_1,
+    text: readTextFile(en_lorem_ipsum)
+  },
+  {
+    name: "Tang Li Qun",
+    avatar: dude_2,
+    text: readTextFile(en_lorem_ipsum)
+  }
+];
+
+const avatars = data => {
+  return data.map(d => {
+    return (
+      <div style={p4Style.avatar_box}>
+        <img style={p4Style.image} src={d.avatar} alt={d.name} />
+        <h2 style={p4Style.title}>{d.name}</h2>
+        <p style={p4Style.text}>{d.text}</p>
+      </div>
+    );
+  });
+};
+
+const page_4 = (
+  <div style={p4Style.root}>
+    {avatars(avatar_data)}
+    <p style={p4Style.footer}>
+      © 2018 some company. <br />
+      <br />
+      terms and conditions
+    </p>
+  </div>
+);
+
+const all_pages = [page_1, page_2, page_3, page_4];
 
 const scrollComponentStyle = {
   display: "flex",
   flexDirection: "column"
 };
 
-export default class Scroll extends Component {
+class Scroll extends Component {
   render() {
     return <div style={scrollComponentStyle}>{all_pages}</div>;
   }
 }
+
+export default Radium(Scroll);
