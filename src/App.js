dceef5eca62728ba2302088725044140083a71c4
panzerstadt <tliqun@gmail.com>
2018-09-05 12:10:47 +0900

    scroll version

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Parallax from "./components/parallax";
+import Scroll from "./components/scroll";
+import Header from "./components/header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Parallax />
+        <Header />
+        <Scroll />
       </div>
     );
   }
