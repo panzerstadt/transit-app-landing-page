e3c20ba802d0d5299d8f4c7d54655cba5c1b0a8a
panzerstadt <tliqun@gmail.com>
2018-09-05 15:31:41 +0900

    flex wrap system

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import en_lorem_ipsum from "../assets/lorem_ipsum/lorem-ipsum-en.txt";
+import ja_lorem_ipsum from "../assets/lorem_ipsum/lorem-ipsum-ja.txt";
+
 function importAll(r) {
   let images = {};
   r.keys().map((item, index) => {
@@ -10,6 +13,23 @@ function importAll(r) {
   return images;
 }
 
+function readTextFile(file) {
+  // https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
+  var rawFile = new XMLHttpRequest();
+  var outFile;
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function() {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        outFile = allText;
+      }
+    }
+  };
+  rawFile.send(null);
+  return outFile;
+}
+
 // prepare bg images
 let images = importAll(require.context("../assets", true, /.*\.jpeg$/));
 
@@ -18,6 +38,7 @@ const p1Style = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    flexWrap: "wrap",
     height: "100vh",
     color: "white",
     paddingTop: 30,
@@ -32,18 +53,18 @@ const p1Style = {
     width: 200
   },
   image: {
-    width: 150
+    width: 300
   }
 };
 
 const page_1 = (
   <div style={p1Style.root}>
-    <h1>test title</h1>
-    <p style={p1Style.text}>
-      test text test text test text test text test text test text
-    </p>
+    <div>
+      <h1>test title</h1>
+      <p style={p1Style.text}>{readTextFile(en_lorem_ipsum)}</p>
+    </div>
+
     <img style={p1Style.image} src={images["bg/man.jpeg"]} />
-    <h1>holy shit</h1>
   </div>
 );
 
@@ -52,6 +73,7 @@ const p2Style = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    flexWrap: "wrap",
     height: "100vh",
     color: "white",
     paddingTop: 30,
@@ -66,16 +88,17 @@ const p2Style = {
     width: 200
   },
   image: {
-    height: 400
+    width: 300
   }
 };
 
 const page_2 = (
   <div style={p2Style.root}>
-    <h1>test title</h1>
-    <p style={p2Style.text}>
-      test text test text test text test text test text test text
-    </p>
+    <div>
+      <h1>test title</h1>
+      <p style={p2Style.text}>{readTextFile(ja_lorem_ipsum)}</p>
+    </div>
+
     <img style={p2Style.image} src={images["bg/man.jpeg"]} />
   </div>
 );
@@ -84,11 +107,14 @@ const p3Style = {
   root: {
     // height: 500,
     display: "flex",
-    justifyContent: "space-between",
-    backgroundColor: "pink"
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    backgroundColor: "white"
   },
   text: {
-    verticalAlign: "middle"
+    fontSize: 20,
+    width: 300
   },
   image: {
     height: "100%",
@@ -100,14 +126,22 @@ const page_3 = (
   <div style={p3Style.root}>
     <img style={p3Style.image} src={images["bg/path.jpeg"]} />
 
-    <p style={p3Style.text}>test page 3</p>
+    <div>
+      <p style={p3Style.text}>test page 3</p>
+      <p style={p3Style.text}>{readTextFile(ja_lorem_ipsum)}</p>
+    </div>
   </div>
 );
 
 const all_pages = [page_1, page_2, page_3, page_2];
 
+const scrollComponentStyle = {
+  display: "flex",
+  flexDirection: "column"
+};
+
 export default class Scroll extends Component {
   render() {
-    return all_pages;
+    return <div style={scrollComponentStyle}>{all_pages}</div>;
   }
 }
