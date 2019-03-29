f548306bb024a320a69dcdfdaa09dc729e682772
panzerstadt <tliqun@gmail.com>
2019-03-29 19:00:10 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9007c4313ff6d9940baa3d7d3e415e4e.js"
+  "https://panzerstadt.github.io/transit-app-landing-page/precache-manifest.1ddefff2457aba0468a3ff9b27d47017.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://panzerstadt.github.io/transit-app-landing-page/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
